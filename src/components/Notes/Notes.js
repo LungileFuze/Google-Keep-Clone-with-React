e1897119658c30e867375d3ab487fcfd7d29fe2c
LungileFuze <ngcobolungile94@outlook.com>
2023-10-06 15:04:47 +0200

    another way of conditional outputting

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -4,18 +4,35 @@ import Note from "./Note";
 const Notes = (props) => {
   console.log("props", props.notes[0]);
   const { notes } = props;
+
+  if(notes.length === 0) {
+    return (
+        <div className="notes">
+            <p>Notes you add appear here.</p>
+        </div>
+    )
+  }
+
   return (
     <div className="notes">
+        {notes.map((note, index) => (
+            <Note key={index} title={note.title} text={note.text} />
+        ))}
+    </div>
+  )
+//   return (
+//     <div className="notes">
         {/* Conditional outputting */}
-        {
+
+        {/* {
             notes.length === 0 && <p>Notes you add appear here.</p>
         }
         {
             notes.length !== 0 && notes.map((note, index) => (
                 <Note key={index} id={note.id} title={note.title} text={note.text} />
             ))
-        }
-
+        } */}
+        // another way
       {/* {notes.length === 0 ? (
         <p>Notes you add appear here.</p>
       ) : (
@@ -23,8 +40,8 @@ const Notes = (props) => {
           <Note key={index} id={note.id} title={note.title} text={note.text} />
         ))
       )} */}
-    </div>
-  );
+    // </div>
+//   );
 };
 
 export default Notes;
